be96245e6c082682903d43f1155f80d5dccd6944
Diba Mirza <dimirza@cs.ucsb.edu>
2018-03-31 17:18:27 -0700

    initial version

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 echo "Installing software needed to run Jekyll locally... "
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
-rvm install ruby-2.3.0
-rvm use 2.3.0
+rvm install ruby-2.3.3
+rvm use 2.3.3
 gem install bundler
 bundle install --path vendor/bundle
 echo "Done."
